8dd76761debf36442d1ea1b9f94b3ce62073741c
DenysSergeev <sergeevdenys@gmail.com>
2022-08-04 11:01:46 +0300

    added store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {configureStore} from 'redux';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+
+
+const defaultState = {
+  cash: 0
+}
+
+const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case "ADD_CASH":
+      return {...state, cash: state.cash + action.payload}
+    case "GET_CASH":
+      return {...state, cash: state.cash - action.payload}
+    default:
+      return state;
+  }
+}
+
+const store = configureStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
